68f6753d3f9ac30a112683bc5e17b0af1d22f71b
Bilal AMRI <iambilalamri@gmail.com>
2021-02-07 18:23:26 +0100

    [master]- add movie categ service

---
+++ movie-catego-service/src/test/java/amrib/service/MovieCategoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package amrib.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCategoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
