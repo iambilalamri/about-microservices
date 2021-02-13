c1b0e87f19db8048df841fff1d63f8ea5a0faeb6
Bilal AMRI <iambilalamri@gmail.com>
2021-02-13 19:55:30 +0100

    [master]-implement microservices

--- movie-catego-service/src/main/java/com/amrib/microservice/service/MovieInfoService.java
+++ movie-catego-service/src/main/java/com/amrib/microservice/service/MovieInfoService.java
@@ -16,11 +16,13 @@ public class MovieInfoService {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem", commandProperties = {
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem", threadPoolKey = "movieInfoPool", commandProperties = {
 			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
 			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
 			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
-			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000") })
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000") }, threadPoolProperties = {
+					@HystrixProperty(name = "coreSize", value = "20"),
+					@HystrixProperty(name = "maxQueueSize", value = "10"), })
 	public CatalogItem getCatalogItem(Rating rating) {
 		Movie movie = restTemplate.getForObject("http://movie-info-service/movie/" + rating.getMovieId(), Movie.class);
 		return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
